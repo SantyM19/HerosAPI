87938c3e2a826ca115e11cd5a598f98950510668
Ana María <anahernandez814@gmail.com>
2021-07-16 16:52:22 -0500

    Se reestructura el front

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,25 +1,9 @@
-import "./App.css";
-import React, {
-  useContext,
-  useReducer,
-  useEffect,
-  useRef,
-  useState,
-  createContext,
-} from "react";
-
+import React, { useContext, useRef, useState } from "react";
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.css";
-import { Navbar } from "react-bootstrap";
-import { reducer } from "./Reducer/Reducer";
+import { Store } from "../App";
 
 
 const HOST_API = "https://app-heroes-of-legends.herokuapp.com";
-const initialState = {
-  list: [],
-};
-
-const Store = createContext(initialState);
 
 const Form = () => {
   const { dispatch } = useContext(Store);
@@ -47,25 +31,49 @@ const Form = () => {
       .catch(function (error) {
         console.log(error);
       });
-
-    /*fetch(HOST_API + "/heroes/crear", {
-      method: "POST",
-      body: request,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((heroes) => {
-        dispatch({ type: "add-item", item: heroes });
-        setState({ nombre: "" });
-        formRef.current.reset();
-      });*/
+      const List = () => {
+        const { dispatch, state } = useContext(Store);
+      
+        useEffect(() => {
+          fetch(HOST_API + "/heroes", {
+            method: "GET",
+          })
+            .then((response) => response.json())
+            .then((list) => {
+              dispatch({ type: "update-list", list });
+            });
+        }, [state.list.length, dispatch]);
+        return (
+          <div className="container flex-column flex-md-row justify-content-center align-items-center h-100 ">
+            <div className="row">
+              {state.list.map((hero) => (
+                <div className="col-md-4">
+                  <div className="tamano-carta card text-center bg-dark animate__animated animate__fadeInUp mx-3 my-3">
+                    <div className="overflow">
+                      <img src={hero.urlImagen} alt="Hero" width="200" height="200"/>
+                    </div>
+                    <div className="card-body text-light">
+                      <h4 className="card-title">{hero.nombre}</h4>
+                      <hr />
+                      <p className="card-text text-white">Raza: {hero.raza}</p>
+                      <p className="card-text text-white">
+                        Habilidad: {hero.habilidad}
+                      </p>
+                      <p className="card-text text-white">
+                        Nivel de poder: {hero.nivelDePoder}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        );
   };
 
   return (
     <form ref={formRef}>
-      <div className="col-md-5 p-4 mx-auto" >
+      <div className="col-md-5 p-4 mx-auto">
         <div className="card ">
           <div className="card-header text-center">
             <h3> Ingresa tu héroe favorito </h3>
@@ -101,7 +109,7 @@ const Form = () => {
                 required
                 onChange={(event) => {
                   setState({ ...state, raza: event.target.value });
-                }} 
+                }}
               />
             </div>
             <div className="mb-3">
@@ -118,7 +126,6 @@ const Form = () => {
                 onChange={(event) => {
                   setState({ ...state, habilidad: [event.target.value] });
                 }}
-                
               />
             </div>
             <div className="mb-3">
@@ -134,7 +141,7 @@ const Form = () => {
                 required
                 onChange={(event) => {
                   setState({ ...state, nivelDePoder: event.target.value });
-                }}   
+                }}
               />
             </div>
             <div className="mb-3">
@@ -169,117 +176,4 @@ const Form = () => {
   );
 };
 
-
-const List = () => {
-  const { dispatch, state } = useContext(Store);
-
-  useEffect(() => {
-    fetch(HOST_API + "/heroes", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((list) => {
-        dispatch({ type: "update-list", list });
-      });
-  }, [state.list.length, dispatch]);
-  return (
-    <div className="container flex-column flex-md-row justify-content-center align-items-center h-100">
-      <div className="row">
-        {state.list.map((hero) => (
-          <div className="col-md-4">
-            <div className="tamano-carta card text-center bg-dark animate__animated animate__fadeInUp mx-3 my-3">
-              <div className="overflow">
-                <img src={hero.urlImagen} alt="Hero" width="200" height="200"/>
-              </div>
-              <div className="card-body text-light">
-                <h4 className="card-title">{hero.nombre}</h4>
-                <hr />
-                <p className="card-text text-white">Raza: {hero.raza}</p>
-                <p className="card-text text-white">
-                  Habilidad: {hero.habilidad}
-                </p>
-                <p className="card-text text-white">
-                  Nivel de poder: {hero.nivelDePoder}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
- 
-  /* return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>NOMBRE</td>
-            <td>RAZA</td>
-            <td>HABILIDAD</td>
-            <td>NIVEL PODER</td>
-            <td>IMAGEN</td>
-          </tr>
-        </thead>
-        <tbody>
-          {state.list.map((hero) => {
-            return (
-              <tr key={hero.id}>
-                <td>{hero.id}</td>
-                <td>{hero.nombre}</td>
-                <td>{hero.raza}</td>
-                <td>{hero.habilidad}</td>
-                <td>{hero.nivelDePoder}</td>
-                <td>
-                  <img
-                    src={hero.urlImagen}
-                    alt="Hero"
-                    width="200"
-                    height="200"
-                  />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  ); */
-};
-
-/*function reducer(state, action) {
-  switch (action.type) {
-    case "update-list":
-      return { ...state, list: action.list };
-    case "add-item":
-      const newList = state.list;
-      newList.push(action.item);
-      return { ...state, list: newList };
-    default:
-      return state;
-  }
-}*/
-
-const StoreProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
-  );
-};
-
-function App() {
-  return (
-    <StoreProvider>
-      <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand> Tú heroe favorito </Navbar.Brand>
-        </Navbar>
-        <Form />
-        <List />
-      </div>
-    </StoreProvider>
-  );
-}
-
-export default App;
+export default Form;
---
+++ src/Reducer/Reducer.js
@@ -0,0 +1,12 @@
+export function reducer(state, action) {
+  switch (action.type) {
+    case "update-list":
+      return { ...state, list: action.list };
+    case "add-item":
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList };
+    default:
+      return state;
+  }
+}
